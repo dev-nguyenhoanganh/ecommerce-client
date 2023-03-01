6bca58733b193b0f492b515e820ff4e714b46f9b
Nguyễn Hoàng Anh <legendarycaptains@gmail.com>
2023-03-01 22:02:38 +0700

    Code Navigation component (#30)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
-import { Header } from './components/layouts';
+import React, { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Header, Navigation } from './components/layouts';
 import ThemeProvider from './theme';
 
 function App() {
+  const [open, setOpen] = useState(true);
+
   return (
     <React.Fragment>
       <ThemeProvider>
-        <Header onOpenNav={() => {}} />
+        <BrowserRouter>
+          <Header onOpenNav={() => {}} />
+          <Navigation openNav={open} onCloseNav={() => setOpen(false)} />
+        </BrowserRouter>
       </ThemeProvider>
     </React.Fragment>
   );
